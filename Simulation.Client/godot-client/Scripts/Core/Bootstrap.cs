0db7b1449badd607b3d0b2548d81a4d092e5209c
filipe <felipe_157@windowslive.com>
2026-01-16 18:31:31 -0400

    refactor: Adjust formatting and indentation in Bootstrap class

--- Simulation.Client/godot-client/Scripts/Core/Bootstrap.cs
+++ Simulation.Client/godot-client/Scripts/Core/Bootstrap.cs
@@ -5,13 +5,13 @@ namespace GodotClient.Core;
 
 public partial class Bootstrap : Node
 {
-    public override void _Ready()
-    {
-        base._Ready();
-        
-        // Carrega a cena do menu principal
-        SceneManager.Instance.LoadMainMenu();
-        
-        GD.Print("[Bootstrap] Game started");
-    }
+	public override void _Ready()
+	{
+		base._Ready();
+		
+		// Carrega a cena do menu principal
+		SceneManager.Instance.LoadMainMenu();
+		
+		GD.Print("[Bootstrap] Game started");
+	}
 }
